f3f1894c08d043d308fab5cde483afbeff63ec10
Eugene Bachura <eugenebachura@gmail.com>
2024-09-03 12:31:39 +0200

    Adding the validateEmail method
    Method to validate email using regular expression

--- UserValidator.php
+++ UserValidator.php
@@ -1,3 +1,22 @@
 <?php
 
-class UserValidator {}
+class UserValidator
+{
+    public function validateEmail(string $email): bool
+    {
+        // Regular expression for email address validation
+        /* 
+            Contains the @ character. Before @ there should be one or more letters;
+            After @ should be a domain (e.g. example.com) that contains a period (.);
+            At least two letters after the period;
+        */
+        $pattern = '/^(?=[^@]*[a-zA-Z])[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
+
+        // Compliance check
+        if (preg_match($pattern, $email)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
