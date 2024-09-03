437667ed286a62580d629d7d98bbcadae85993b4
Eugene Bachura <eugenebachura@gmail.com>
2024-09-03 14:11:58 +0200

    Code refactoring

--- UserValidator.php
+++ UserValidator.php
@@ -6,9 +6,9 @@ class UserValidator
     {
         // Regular expression for email address validation
         /* 
-            Contains the @ character. Before @ there should be one or more letters;
-            After @ should be a domain (e.g. example.com) that contains a period (.);
-            At least two letters after the period;
+            Contains the @ character. Before @ there should be one or more letters: (?=[^@]*[a-zA-Z]);
+            After @ should be a domain (e.g. example.com) that contains a dot (.);
+            At least two letters after the dot;
         */
         $pattern = '/^(?=[^@]*[a-zA-Z])[\w.%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/';
 
